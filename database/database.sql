3b63002625fa26ae437b9ff947dc46def266adeb
Hayyu <nurhayyuf@gmail.com>
2026-06-08 10:41:34 +0700

    update

--- database/database.sql
+++ database/database.sql
@@ -19,31 +19,6 @@
 CREATE DATABASE IF NOT EXISTS `medisecure` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `medisecure`;
 
--- Dumping structure for table medisecure.activity_logs
-CREATE TABLE IF NOT EXISTS `activity_logs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `activity` text,
-  `ip_address` varchar(50) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `activity_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table medisecure.activity_logs: ~0 rows (approximately)
-
--- Dumping structure for table medisecure.csrf_logs
-CREATE TABLE IF NOT EXISTS `csrf_logs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `ip_address` varchar(50) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table medisecure.csrf_logs: ~0 rows (approximately)
-
 -- Dumping structure for table medisecure.dokter
 CREATE TABLE IF NOT EXISTS `dokter` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -55,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `dokter` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `dokter_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table medisecure.dokter: ~0 rows (approximately)
 
@@ -70,21 +45,9 @@ CREATE TABLE IF NOT EXISTS `hasil_lab` (
   PRIMARY KEY (`id`),
   KEY `pasien_id` (`pasien_id`),
   CONSTRAINT `hasil_lab_ibfk_1` FOREIGN KEY (`pasien_id`) REFERENCES `pasien` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table medisecure.hasil_lab: ~0 rows (approximately)
-
--- Dumping structure for table medisecure.login_attempts
-CREATE TABLE IF NOT EXISTS `login_attempts` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) DEFAULT NULL,
-  `ip_address` varchar(50) DEFAULT NULL,
-  `status` enum('success','failed') DEFAULT NULL,
-  `attempt_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table medisecure.login_attempts: ~0 rows (approximately)
+-- Dumping data for table medisecure.hasil_lab: ~1 rows (approximately)
 
 -- Dumping structure for table medisecure.pasien
 CREATE TABLE IF NOT EXISTS `pasien` (
@@ -99,9 +62,9 @@ CREATE TABLE IF NOT EXISTS `pasien` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `pasien_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table medisecure.pasien: ~0 rows (approximately)
+-- Dumping data for table medisecure.pasien: ~1 rows (approximately)
 
 -- Dumping structure for table medisecure.rekam_medis
 CREATE TABLE IF NOT EXISTS `rekam_medis` (
@@ -117,21 +80,9 @@ CREATE TABLE IF NOT EXISTS `rekam_medis` (
   KEY `dokter_id` (`dokter_id`),
   CONSTRAINT `rekam_medis_ibfk_1` FOREIGN KEY (`pasien_id`) REFERENCES `pasien` (`id`) ON DELETE CASCADE,
   CONSTRAINT `rekam_medis_ibfk_2` FOREIGN KEY (`dokter_id`) REFERENCES `dokter` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table medisecure.rekam_medis: ~0 rows (approximately)
-
--- Dumping structure for table medisecure.security_alerts
-CREATE TABLE IF NOT EXISTS `security_alerts` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `source` varchar(50) DEFAULT NULL,
-  `severity` enum('low','medium','high','critical') DEFAULT NULL,
-  `alert_message` text,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table medisecure.security_alerts: ~0 rows (approximately)
 
 -- Dumping structure for table medisecure.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -145,43 +96,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table medisecure.users: ~2 rows (approximately)
-REPLACE INTO `users` (`id`, `nama`, `email`, `password`, `role`, `is_active`, `created_at`, `updated_at`) VALUES
-	(1, 'Administrator', 'admin@medisecure.com', '$2y$10$ISI_HASH_HASIL_PASSWORD_HASH', 'admin', 1, '2026-06-07 20:47:20', '2026-06-07 20:47:20'),
-	(2, 'Ubad', 'Ubad@gmail.com', '$2y$10$SdqCvklEHZR1FHQQwQqgd.IHvDiXm7GBa4IqDLE2oGEcvFkpkP8ZW', 'pasien', 1, '2026-06-07 21:06:23', '2026-06-07 21:06:23');
-
--- Dumping structure for table medisecure.user_otp
-CREATE TABLE IF NOT EXISTS `user_otp` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `otp_code` varchar(10) DEFAULT NULL,
-  `expired_at` datetime DEFAULT NULL,
-  `is_used` tinyint(1) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `user_otp_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table medisecure.user_otp: ~0 rows (approximately)
-
--- Dumping structure for table medisecure.user_sessions
-CREATE TABLE IF NOT EXISTS `user_sessions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `session_id` varchar(255) DEFAULT NULL,
-  `ip_address` varchar(50) DEFAULT NULL,
-  `user_agent` text,
-  `login_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `logout_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `user_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table medisecure.user_sessions: ~0 rows (approximately)
+-- Dumping data for table medisecure.users: ~3 rows (approximately)
+INSERT INTO `users` (`id`, `nama`, `email`, `password`, `role`, `is_active`, `created_at`, `updated_at`) VALUES
+	(15, 'Admin', 'admin@medisecure.com', '$2y$10$oOuhVmQ94YDK56/Bf/39o./Ht8TFLkvVT813WFdA8RftDUW3s51V.', 'admin', 1, '2026-06-08 03:23:30', '2026-06-08 03:33:46'),
+	(16, 'Dokter', 'dokter@medisecure.com', '$2y$10$K4d5OLX1U58voC6ORVSb9uMl00fh4lhpVagKHZ.3YPB4R2TPPDcje', 'dokter', 1, '2026-06-08 03:23:47', '2026-06-08 03:35:27'),
+	(17, 'Pasien', 'hayyu@gmail.com', '$2y$10$TrV6Cm5GJZ15NVnmrHhfNeS6hvUfA7uTPRYrUa3Q5iQJzKQ32sV.y', 'pasien', 1, '2026-06-08 03:25:31', '2026-06-08 03:38:31');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
